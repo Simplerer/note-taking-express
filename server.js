93ca0434042cf9857abca3249374875285a5cef3
A Stone <simplererstone@gmail.com>
2023-01-05 13:24:34 -0500

    following class

--- server.js
+++ server.js
@@ -1,34 +1,47 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const uuid = require('./Assets/helpers/uuid');
+const PORT = process.env.PORT || 3001;
 
-PORT = 3001;
-app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
+const app = express();
 
 app.get('/', (req, res) => {
-
+  res.send('Note Taker');
 });
 
-app.get('/notes', (req, res) => {
-
-});
-
-app.get('/api/notes', (req, res) => {
-    res.json(`${req.method} request recieved!`);
-    console.info(`${req.method} request recieved!`)
-});
-
-app.post('/api/reviews', (req, res) => {
-
-});
+app.listen(PORT);
 
 
-app.listen(PORT, () => {
-    console.log(`Listening at http://localhost:${PORT}`);
-})
+
+// const express = require('express');
+// const path = require('path');
+// const fs = require('fs');
+// const uuid = require('./helpers/uuid');
+
+// PORT = 3001;
+// app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'));
+
+
+// app.get('/', (req, res) => {
+
+// });
+
+// app.get('/notes', (req, res) => {
+
+// });
+
+// app.get('/api/notes', (req, res) => {
+//     res.json(`${req.method} request recieved!`);
+//     console.info(`${req.method} request recieved!`)
+// });
+
+// app.post('/api/reviews', (req, res) => {
+
+// });
+
+
+// app.listen(PORT, () => {
+//     console.log(`Listening at http://localhost:${PORT}`);
+// })
