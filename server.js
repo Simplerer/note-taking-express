389530df79578fd0862f89c2a807dee61f6dc77f
A Stone <simplererstone@gmail.com>
2023-01-06 16:41:01 -0500

    notes delete but not untill reload

--- server.js
+++ server.js
@@ -40,25 +40,35 @@ app.post('/api/notes', (req, res) => {
         parsedNotes.push(newNote);
         const noteList = JSON.stringify(parsedNotes, null, 2);
 
-        fs.writeFile(`./db/db.json`, noteList, (err) => console.error(err));
-
+        fs.writeFile(`./db/db.json`, noteList, (err) => err ? console.error(err) : console.log('Note Saved!'));
+        
         const response = {
             status: 'success',
             body: newNote,
         };
-
+        
         res.json(response);
     } else {
         res.status(500).json(`Note didn't take!`)
     };
-
+    
 });
 
 app.delete(`/api/notes/:id`, (req, res) => {
     const { id } = req.params;
+    
+    const savedNotes = fs.readFileSync('./db/db.json', 'utf-8');
+    const parsedNotes = JSON.parse(savedNotes);
 
-
-    console.info('deleted that pesky note!');               // we can pop off db.json and re-write file!
+    const oneLessNote = parsedNotes.filter((notes) => {
+       if (notes.id !== id) {
+        return;
+       };
+    });
+    
+    const noteList = JSON.stringify(oneLessNote, null, 2);
+    fs.writeFile(`./db/db.json`, noteList, (err) => err ? console.error(err) : console.log('deleted that pesky note!'));
+    
     res.status(202);
 });
 
