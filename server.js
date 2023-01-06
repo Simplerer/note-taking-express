864648bd0280171d67693758734a98111c60ea36
A Stone <simplererstone@gmail.com>
2023-01-05 23:21:16 -0500

    setting up note persistence

--- server.js
+++ server.js
@@ -1,43 +1,51 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-
-// const app = express();
-
-// app.get('/', (req, res) => {
-//   res.send('Note Taker');
-// });
-
-// app.listen(PORT);
-
-
-
-// const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
+const db = require('./db/db.json')
 
-PORT = 3001;
-app = express();
+// const PORT = 3001;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
+//index - static
 app.get('/', (req, res) => {
-
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+//notes - static in public folder
 app.get('/notes', (req, res) => {
-
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
+
 
 app.get('/api/notes', (req, res) => {
-    res.json(`${req.method} request recieved!`);
-    console.info(`${req.method} request recieved!`)
+    res.json(db);    
 });
 
+app.post('/api/notes', (req, res) => {
+    
+    console.log(`${req.method} request to add note recieved`)
+    console.info(`${req.method} request to add note recieved`)
+    
+})
+
 app.post('/api/reviews', (req, res) => {
+    
+
+    const { text, title } = req.body;
+
+    if (text && title) {
+        const newNote = {
+            text,
+            title,
+
+        }
+    }
 
 });
 
